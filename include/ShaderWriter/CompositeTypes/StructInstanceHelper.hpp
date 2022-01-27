84140a13121f951fe7db6dcd9a7664f1411f279e
DragonJoker <dragonjoker59@hotmail.com>
2022-01-27 15:47:28 +0100

    Added StructInstanceHelper and tests.

---
+++ include/ShaderWriter/CompositeTypes/StructInstanceHelper.hpp
@@ -0,0 +1,104 @@
+/*
+See LICENSE file in root folder
+*/
+#ifndef ___SDW_StructInstanceHelper_H___
+#define ___SDW_StructInstanceHelper_H___
+#pragma once
+
+namespace sdw
+{
+	template< StringLiteralT StructNameT
+		, ast::type::MemoryLayout LayoutT
+		, typename... FieldsT >
+	class StructInstanceHelperT
+		: public StructInstance
+	{
+		static_assert( sizeof...( FieldsT ) > 0
+			, "A structure must have at least one field" );
+
+	public:
+		~StructInstanceHelperT() override = default;
+		StructInstanceHelperT( StructInstanceHelperT const & ) = default;
+		StructInstanceHelperT( StructInstanceHelperT && ) = default;
+
+		StructInstanceHelperT( ShaderWriter & writer
+			, ast::expr::ExprPtr expr
+			, bool enabled = true )
+			: StructInstance{ writer, std::move( expr ), enabled }
+		{
+		}
+
+		StructInstanceHelperT & operator=( StructInstanceHelperT const & rhs )
+		{
+			StructInstance::operator=( rhs );
+			return *this;
+		}
+
+		StructInstanceHelperT & operator=( StructInstanceHelperT && rhs )
+		{
+			StructInstance::operator=( std::move( rhs ) );
+			return *this;
+		}
+
+		template< sdw::StringLiteralT FieldNameT >
+		constexpr static auto getFieldByName()
+		{
+			return doGetFieldByName<FieldNameT, FieldsT...>();
+		}
+
+		template< sdw::StringLiteralT FieldNameT >
+		constexpr static bool hasFieldByName()
+		{
+			return ( ... || ( std::string_view( FieldNameT.value ) == FieldsT::Name ) );
+		}
+
+		template< sdw::StringLiteralT FieldNameT >
+		auto getMember()
+		{
+			static_assert( hasFieldByName< FieldNameT >() );
+			using FieldT = decltype( getFieldByName< FieldNameT >() );
+			return FieldT::template get( *this );
+		}
+
+		static type::BaseStructPtr makeType( type::TypesCache & cache )
+		{
+			auto result = cache.getStruct( LayoutT
+				, std::string( StructNameT.value ) );
+
+			if ( result->empty() )
+			{
+				( result->declMember( std::string( FieldsT::Name )
+					, typeEnum< typename FieldsT::TypeT >
+					, FieldsT::ArraySize )
+					, ... );
+			}
+
+			return result;
+		}
+
+		using StructInstance::getMember;
+		using StructInstance::getMemberArray;
+
+	private:
+		template< sdw::StringLiteralT FieldNameT >
+		constexpr static auto doGetFieldByName()
+		{
+			return sdw::Void{};
+		}
+
+		template< sdw::StringLiteralT FieldNameT, typename FieldU, typename... FieldsU >
+		constexpr static auto doGetFieldByName()
+		{
+			if constexpr ( std::string_view( FieldNameT.value ) == FieldU::Name )
+			{
+				return FieldU{};
+			}
+			else
+			{
+				return doGetFieldByName< FieldNameT, FieldsU... >();
+			}
+		}
+	};
+}
+
+#endif
--- test/ShaderWriter/TestWriterCustomType.cpp
+++ test/ShaderWriter/TestWriterCustomType.cpp
@@ -79,7 +79,36 @@ namespace
 		using sdw::StructInstance::getMemberArray;
 	};
 
+	struct Light2
+		: public sdw::StructInstanceHelperT< "Light"
+			, ast::type::MemoryLayout::eStd140
+			, sdw::StructFieldT< sdw::Vec4, "colorIntensity" > >
+	{
+		Light2( sdw::ShaderWriter & writer, ast::expr::ExprPtr expr, bool enabled )
+			: StructInstanceHelperT{ writer, std::move( expr ), enabled }
+		{
+		}
+
+		static ast::type::BaseStructPtr makeType( ast::type::TypesCache & cache )
+		{
+			auto result = cache.getStruct( ast::type::MemoryLayout::eStd140
+				, "Light" );
+
+			if ( result->empty() )
+			{
+				result->declMember( "colorIntensity", ast::type::Kind::eVec4F );
+			}
+
+			return result;
+		}
+
+		auto colorIntensity() { return getMember< "colorIntensity" >(); }
+		auto color() { return colorIntensity().xyz(); }
+		auto intensity() { return colorIntensity().w(); }
+	};
+
 	Writer_Parameter( Light );
+	Writer_Parameter( Light2 );
 }
 
 namespace
@@ -98,10 +127,7 @@ namespace
 				} );
 			test::writeShader( writer
 				, testCounts
-				, Compilers_NoSPIRV );
-			test::writeShader( writer
-				, testCounts
-				, Compilers_SPIRV );
+				, CurrentCompilers );
 			shaders.emplace_back( std::move( writer.getShader() ) );
 		}
 		{
@@ -118,10 +144,7 @@ namespace
 				} );
 			test::writeShader( writer
 				, testCounts
-				, Compilers_NoSPIRV );
-			test::writeShader( writer
-				, testCounts
-				, Compilers_SPIRV );
+				, CurrentCompilers );
 			shaders.emplace_back( std::move( writer.getShader() ) );
 		}
 		test::validateShaders( shaders
@@ -150,10 +173,7 @@ namespace
 			} );
 		test::writeShader( writer
 			, testCounts
-			, Compilers_NoSPIRV );
-		test::writeShader( writer
-			, testCounts
-			, Compilers_SPIRV );
+			, CurrentCompilers );
 		testEnd();
 	}
 
@@ -171,10 +191,7 @@ namespace
 				} );
 			test::writeShader( writer
 				, testCounts
-				, Compilers_NoSPIRV );
-			test::writeShader( writer
-				, testCounts
-				, Compilers_SPIRV );
+				, CurrentCompilers );
 			shaders.emplace_back( std::move( writer.getShader() ) );
 		}
 		{
@@ -191,10 +208,7 @@ namespace
 				} );
 			test::writeShader( writer
 				, testCounts
-				, Compilers_NoSPIRV );
-			test::writeShader( writer
-				, testCounts
-				, Compilers_SPIRV );
+				, CurrentCompilers );
 			shaders.emplace_back( std::move( writer.getShader() ) );
 		}
 		test::validateShaders( shaders
@@ -223,10 +237,7 @@ namespace
 			} );
 		test::writeShader( writer
 			, testCounts
-			, Compilers_NoSPIRV );
-		test::writeShader( writer
-			, testCounts
-			, Compilers_SPIRV );
+			, CurrentCompilers );
 		testEnd();
 	}
 
@@ -246,10 +257,155 @@ namespace
 			} );
 		test::writeShader( writer
 			, testCounts
-			, Compilers_NoSPIRV );
+			, CurrentCompilers );
+		testEnd();
+	}
+
+	void singleLight2Ubo( test::sdw_test::TestCounts & testCounts )
+	{
+		testBegin( "singleLight2Ubo" );
+		using namespace sdw;
+		sdw::ShaderArray shaders;
+		{
+			VertexWriter writer;
+			writer.implementMainT< VoidT, VoidT >( [&]( VertexIn in
+				, VertexOut out )
+				{
+					out.vtx.position = vec4( 1.0_f );
+				} );
+			test::writeShader( writer
+				, testCounts
+				, CurrentCompilers );
+			shaders.emplace_back( std::move( writer.getShader() ) );
+		}
+		{
+			FragmentWriter writer;
+			writer.declType< Light2 >();
+			auto lightUbo = writer.declUniformBuffer<>( "Light2Ubo", 0u, 0u );
+			auto light = lightUbo.declMember< Light2 >( "light" );
+			lightUbo.end();
+
+			writer.implementMainT< VoidT, ColourT >( [&]( FragmentInT< VoidT > in
+				, FragmentOutT< ColourT > out )
+				{
+					out.colour = light.color() * light.intensity();
+				} );
+			test::writeShader( writer
+				, testCounts
+				, CurrentCompilers );
+			shaders.emplace_back( std::move( writer.getShader() ) );
+		}
+		test::validateShaders( shaders
+			, testCounts
+			, CurrentCompilers );
+		testEnd();
+	}
+		
+	void light2ArrayUbo( test::sdw_test::TestCounts & testCounts )
+	{
+		testBegin( "light2ArrayUbo" );
+		using namespace sdw;
+
+		FragmentWriter writer;
+
+		writer.declType< Light2 >();
+		auto lightsUbo = writer.declUniformBuffer<>( "Light2sUbo", 0u, 0u );
+		auto lights = lightsUbo.declMember< Light2 >( "lights"
+			, 2u );
+		lightsUbo.end();
+
+		writer.implementMainT< VoidT, ColourT >( [&]( FragmentInT< VoidT > in
+			, FragmentOutT< ColourT > out )
+			{
+				out.colour = lights[0].color() * lights[1].intensity();
+			} );
 		test::writeShader( writer
 			, testCounts
-			, Compilers_SPIRV );
+			, CurrentCompilers );
+		testEnd();
+	}
+
+	void singleLight2Ssbo( test::sdw_test::TestCounts & testCounts )
+	{
+		testBegin( "singleLight2Ssbo" );
+		using namespace sdw;
+		sdw::ShaderArray shaders;
+		{
+			VertexWriter writer;
+			writer.implementMainT< VoidT, VoidT >( [&]( VertexIn in
+				, VertexOut out )
+				{
+					out.vtx.position = vec4( 1.0_f );
+				} );
+			test::writeShader( writer
+				, testCounts
+				, CurrentCompilers );
+			shaders.emplace_back( std::move( writer.getShader() ) );
+		}
+		{
+			FragmentWriter writer;
+			writer.declType< Light2 >();
+			auto lightSsbo = writer.declUniformBuffer<>( "Light2Ssbo", 1u, 0u, type::MemoryLayout::eStd140 );
+			auto light = lightSsbo.declMember< Light2 >( "light" );
+			lightSsbo.end();
+
+			writer.implementMainT< VoidT, ColourT >( [&]( FragmentInT< VoidT > in
+				, FragmentOutT< ColourT > out )
+				{
+					out.colour = light.color() * light.intensity();
+				} );
+			test::writeShader( writer
+				, testCounts
+				, CurrentCompilers );
+			shaders.emplace_back( std::move( writer.getShader() ) );
+		}
+		test::validateShaders( shaders
+			, testCounts
+			, CurrentCompilers );
+		testEnd();
+	}
+
+	void light2ArraySsbo( test::sdw_test::TestCounts & testCounts )
+	{
+		testBegin( "light2ArraySsbo" );
+		using namespace sdw;
+
+		FragmentWriter writer;
+
+		writer.declType< Light2 >();
+		auto lightsSsbo = writer.declUniformBuffer<>( "Light2sSsbo", 1u, 0u, type::MemoryLayout::eStd140 );
+		auto lights = lightsSsbo.declMember< Light2 >( "lights"
+			, 2u );
+		lightsSsbo.end();
+
+		writer.implementMainT< VoidT, ColourT >( [&]( FragmentInT< VoidT > in
+			, FragmentOutT< ColourT > out )
+			{
+				out.colour = lights[0].color() * lights[1].intensity();
+			} );
+		test::writeShader( writer
+			, testCounts
+			, CurrentCompilers );
+		testEnd();
+	}
+
+	void arraySsboLight2( test::sdw_test::TestCounts & testCounts )
+	{
+		testBegin( "arraySsboLight2" );
+		using namespace sdw;
+
+		FragmentWriter writer;
+
+		auto lights = writer.declArrayShaderStorageBuffer< Light2 >( "Light2sSsbo", 1u, 0u );
+
+		writer.implementMainT< VoidT, ColourT >( [&]( FragmentInT< VoidT > in
+			, FragmentOutT< ColourT > out )
+			{
+				out.colour = lights[0].color() * lights[1].intensity();
+			} );
+		test::writeShader( writer
+			, testCounts
+			, CurrentCompilers );
 		testEnd();
 	}
 }
@@ -262,6 +418,11 @@ sdwTestSuiteMain( TestWriterCustomType )
 	singleLightSsbo( testCounts );
 	lightArraySsbo( testCounts );
 	arraySsboLight( testCounts );
+	singleLight2Ubo( testCounts );
+	light2ArrayUbo( testCounts );
+	singleLight2Ssbo( testCounts );
+	light2ArraySsbo( testCounts );
+	arraySsboLight2( testCounts );
 	sdwTestSuiteEnd();
 }
 
