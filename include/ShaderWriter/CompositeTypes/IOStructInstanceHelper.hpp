cafac6221f8e4aa5f2ed9f73a461f2dd7c5f16f0
Sylvain Doremus <DragonJoker@users.noreply.github.com>
2022-11-04 17:27:04 +0100

    ShaderWriter: Fixed handling of struct arrays in struct helpers.

--- include/ShaderWriter/CompositeTypes/IOStructInstanceHelper.hpp
+++ include/ShaderWriter/CompositeTypes/IOStructInstanceHelper.hpp
@@ -72,7 +72,7 @@ namespace sdw
 			if ( result->empty() )
 			{
 				( result->declMember( std::string( FieldsT::Name )
-					, typeEnumV< typename FieldsT::TypeT >
+					, FieldsT::TypeT::makeType( cache )
 					, FieldsT::ArraySize
 					, FieldsT::Location )
 					, ... );
