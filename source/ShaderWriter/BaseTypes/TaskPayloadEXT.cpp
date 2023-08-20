591966043be496afe0fa15d64c233803266113be
Sylvain Doremus <dragonjoker59@hotmail.com>
2023-08-20 15:41:11 +0200

    ShaderWriter: Fixed NV mesh and EXT mesh shaders consistency

--- source/ShaderWriter/BaseTypes/TaskPayloadEXT.cpp
+++ source/ShaderWriter/BaseTypes/TaskPayloadEXT.cpp
@@ -2,6 +2,8 @@
 See LICENSE file in root folder
 */
 #include "ShaderWriter/BaseTypes/TaskPayloadEXT.hpp"
+
+#include "ShaderWriter/Writer.hpp"
 #include "ShaderWriter/Intrinsics/IntrinsicFunctions.hpp"
 
 namespace sdw
@@ -13,6 +15,16 @@ namespace sdw
 		, m_plExpr{ expr }
 		, m_plEnabled{ enabled }
 	{
+	}
+
+	void TaskPayloadEXT::dispatchMesh( UInt32 taskCountX
+		, UInt32 taskCountY
+		, UInt32 taskCountZ )const
+	{
+		getWriter()->addStmt( makeDispatchMesh( makeExpr( taskCountX )
+			, makeExpr( taskCountY )
+			, makeExpr( taskCountZ )
+			, makeExpr( *this ) ) );
 	}
 
 	expr::ExprPtr makeExpr( TaskPayloadEXT const & value )
