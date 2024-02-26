c3c6abfdd46730ba084433c48567667ebf638eb3
Sylvain Doremus <dragonjoker59@hotmail.com>
2024-02-26 10:14:50 +0100

    Added codecov.yml

--- source/VulkanLayer/VulkanLayer.cpp
+++ source/VulkanLayer/VulkanLayer.cpp
@@ -122,7 +122,7 @@ namespace ast::vk
 		case VK_ERROR_INVALID_VIDEO_STD_PARAMETERS_KHR: stream << "The specified Video Std parameters do not adhere to the syntactic or semantic requirements of the used video compression standard, or values derived from parameters according to the rules defined by the used video compression standard do not adhere to the capabilities of the video compression standard or the implementation."; break;
 #endif
 #if VK_EXT_shader_object
-		case VK_ERROR_INCOMPATIBLE_SHADER_BINARY_EXT: stream << "TThe provided binary shader code is not compatible with this device"; break;
+		case VK_ERROR_INCOMPATIBLE_SHADER_BINARY_EXT: stream << "The provided binary shader code is not compatible with this device"; break;
 #endif
 		case VK_RESULT_MAX_ENUM: stream << "vk_result_max_enum"; break;
 		}
