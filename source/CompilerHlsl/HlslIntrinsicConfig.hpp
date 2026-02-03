f11406e5bb2b7793734ef727b79e5b0dffa6d281
Sylvain Doremus <dragonjoker59@hotmail.com>
2026-02-03 15:38:46 +0100

    HLSL: Fixed handling of some missing wave intrinsics.

--- source/CompilerHlsl/HlslIntrinsicConfig.hpp
+++ source/CompilerHlsl/HlslIntrinsicConfig.hpp
@@ -636,6 +636,28 @@ namespace hlsl
 		case ast::expr::Intrinsic::eSubgroupBroadcastFirst3D:
 		case ast::expr::Intrinsic::eSubgroupBroadcastFirst4D:
 		case ast::expr::Intrinsic::eSubgroupBallot:
+		case ast::expr::Intrinsic::eSubgroupBallotBitCount:
+		case ast::expr::Intrinsic::eSubgroupBallotExclusiveBitCount:
+		case ast::expr::Intrinsic::eSubgroupShuffle1F:
+		case ast::expr::Intrinsic::eSubgroupShuffle2F:
+		case ast::expr::Intrinsic::eSubgroupShuffle3F:
+		case ast::expr::Intrinsic::eSubgroupShuffle4F:
+		case ast::expr::Intrinsic::eSubgroupShuffle1I:
+		case ast::expr::Intrinsic::eSubgroupShuffle2I:
+		case ast::expr::Intrinsic::eSubgroupShuffle3I:
+		case ast::expr::Intrinsic::eSubgroupShuffle4I:
+		case ast::expr::Intrinsic::eSubgroupShuffle1U:
+		case ast::expr::Intrinsic::eSubgroupShuffle2U:
+		case ast::expr::Intrinsic::eSubgroupShuffle3U:
+		case ast::expr::Intrinsic::eSubgroupShuffle4U:
+		case ast::expr::Intrinsic::eSubgroupShuffle1B:
+		case ast::expr::Intrinsic::eSubgroupShuffle2B:
+		case ast::expr::Intrinsic::eSubgroupShuffle3B:
+		case ast::expr::Intrinsic::eSubgroupShuffle4B:
+		case ast::expr::Intrinsic::eSubgroupShuffle1D:
+		case ast::expr::Intrinsic::eSubgroupShuffle2D:
+		case ast::expr::Intrinsic::eSubgroupShuffle3D:
+		case ast::expr::Intrinsic::eSubgroupShuffle4D:
 		case ast::expr::Intrinsic::eSubgroupAdd1F:
 		case ast::expr::Intrinsic::eSubgroupAdd2F:
 		case ast::expr::Intrinsic::eSubgroupAdd3F:
@@ -971,6 +993,66 @@ namespace hlsl
 		case ast::expr::Intrinsic::eSubgroupQuadBroadcast2D:
 		case ast::expr::Intrinsic::eSubgroupQuadBroadcast3D:
 		case ast::expr::Intrinsic::eSubgroupQuadBroadcast4D:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal1F:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal2F:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal3F:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal4F:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal1I:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal2I:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal3I:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal4I:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal1U:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal2U:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal3U:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal4U:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal1B:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal2B:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal3B:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal4B:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal1D:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal2D:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal3D:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapHorizontal4D:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical1F:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical2F:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical3F:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical4F:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical1I:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical2I:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical3I:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical4I:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical1U:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical2U:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical3U:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical4U:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical1B:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical2B:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical3B:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical4B:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical1D:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical2D:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical3D:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapVertical4D:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal1F:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal2F:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal3F:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal4F:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal1I:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal2I:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal3I:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal4I:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal1U:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal2U:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal3U:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal4U:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal1B:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal2B:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal3B:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal4B:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal1D:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal2D:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal3D:
+		case ast::expr::Intrinsic::eSubgroupQuadSwapDiagonal4D:
 			config.requiresWaveOps = true;
 			break;
 			
@@ -1023,32 +1105,10 @@ namespace hlsl
 		case ast::expr::Intrinsic::eAtomicExchange2H:
 		case ast::expr::Intrinsic::eAtomicExchange4H:
 		case ast::expr::Intrinsic::eSubgroupInverseBallot:
-		case ast::expr::Intrinsic::eSubgroupBallotBitCount:
 		case ast::expr::Intrinsic::eSubgroupBallotBitExtract:
-		case ast::expr::Intrinsic::eSubgroupBallotExclusiveBitCount:
 		case ast::expr::Intrinsic::eSubgroupBallotInclusiveBitCount:
 		case ast::expr::Intrinsic::eSubgroupBallotFindLSB:
 		case ast::expr::Intrinsic::eSubgroupBallotFindMSB:
-		case ast::expr::Intrinsic::eSubgroupShuffle1F:
-		case ast::expr::Intrinsic::eSubgroupShuffle2F:
-		case ast::expr::Intrinsic::eSubgroupShuffle3F:
-		case ast::expr::Intrinsic::eSubgroupShuffle4F:
-		case ast::expr::Intrinsic::eSubgroupShuffle1I:
-		case ast::expr::Intrinsic::eSubgroupShuffle2I:
-		case ast::expr::Intrinsic::eSubgroupShuffle3I:
-		case ast::expr::Intrinsic::eSubgroupShuffle4I:
-		case ast::expr::Intrinsic::eSubgroupShuffle1U:
-		case ast::expr::Intrinsic::eSubgroupShuffle2U:
-		case ast::expr::Intrinsic::eSubgroupShuffle3U:
-		case ast::expr::Intrinsic::eSubgroupShuffle4U:
-		case ast::expr::Intrinsic::eSubgroupShuffle1B:
-		case ast::expr::Intrinsic::eSubgroupShuffle2B:
-		case ast::expr::Intrinsic::eSubgroupShuffle3B:
-		case ast::expr::Intrinsic::eSubgroupShuffle4B:
-		case ast::expr::Intrinsic::eSubgroupShuffle1D:
-		case ast::expr::Intrinsic::eSubgroupShuffle2D:
-		case ast::expr::Intrinsic::eSubgroupShuffle3D:
-		case ast::expr::Intrinsic::eSubgroupShuffle4D:
 		case ast::expr::Intrinsic::eSubgroupShuffleXor1F:
 		case ast::expr::Intrinsic::eSubgroupShuffleXor2F:
 		case ast::expr::Intrinsic::eSubgroupShuffleXor3F:
